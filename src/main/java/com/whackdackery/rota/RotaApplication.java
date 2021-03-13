cdcdfcb6789abfd9c284d611b93569f6e81861ee
chrissearle <chris.searle@brightpearl.com>
2021-03-13 16:43:17 +0000

    git init

---
+++ src/main/java/com/whackdackery/rota/RotaApplication.java
@@ -0,0 +1,13 @@
+package com.whackdackery.rota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RotaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whackdackery/rota/RotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whackdackery.rota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
