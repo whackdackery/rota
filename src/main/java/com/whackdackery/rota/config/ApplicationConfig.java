501b496bb32302c2e132420915d8039caabbcbe7
chrissearle <chris.searle@brightpearl.com>
2021-03-14 19:37:12 +0000

    Move db credentials into env vars

--- src/main/java/com/whackdackery/rota/config/ApplicationConfig.java
+++ src/main/java/com/whackdackery/rota/config/ApplicationConfig.java
@@ -3,11 +3,8 @@ package com.whackdackery.rota.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@PropertySource("classpath:database-credentials.properties")
-@PropertySource("classpath:flyway-credentials.properties")
 public class ApplicationConfig {
 
     @Bean
